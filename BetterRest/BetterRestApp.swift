786ed375b64cfee2bf07d2cc6ba2aa7261ce90b2
Mweh <fahmianton@gmail.com>
2023-08-26 22:39:44 +0700

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Muhammad Fahmi on 24/08/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
